38533fed2485cfac7d49198723dc6de6bd79f2e3
happyjy <okwoyjy@gmail.com>
2023-04-30 14:47:55 +0900

    csr 학습

--- nextjs-blog/pages/CSR.js
+++ nextjs-blog/pages/CSR.js
@@ -1,26 +1,25 @@
 import Head from "next/head";
-import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
 
 /* 
-  ⭐️point: SSR 설명 - 서버에서 데이터를 가지고 오고 페이지로 전달하는 역할을 한다.
-    - SSR (Server Side Render)
-    - 서버가 그린다
+  ⭐️point: CSR(Client Side Render) 설명
+    - 이페이지를 확인하려면 "http://localhost:3000/CSR" 여기로 접속해야 합니다. 
+    - 클라이언트가 그린다
     - 그린다: 데이터를 가져와서 그린다
-    - 서버가 그린다: 서버가 데이터를 가져와서 그린다
-    - SSR을 담당하는 함수 -> getServerSideProps
+    - 클라이언트가 그린다: 클라이언트가 데이터를 가져와서 그린다
+    - CSR을 담당하는 함수 따로 없음
+    - 기존 React 사용법과 동일
 */
-export async function getServerSideProps() {
-  console.log("--- server");
-  return {
-    //⭐️ step-1
-    props: { time: new Date().toISOString() },
-  };
-}
 
-export default function Home({ time }) {
-  console.log("time: ", time);
-  //⭐️ step-2
+export default function CSR() {
+  const [time, setTime] = useState();
+
+  useEffect(() => {
+    console.log("client");
+    setTime(new Date().toISOString());
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -30,10 +29,7 @@ export default function Home({ time }) {
 
       <main>
         <h1 className="title">{time}</h1>
-        <label>ssr 예시 페이지 입니다.</label>
-        <h1>
-          <Link href="/CSR">CSR 예시 페이지로</Link>
-        </h1>
+        <label>csr 예시 페이지 입니다.</label>
       </main>
 
       <footer>
