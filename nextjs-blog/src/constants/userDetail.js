65cdef278b6ed559b4671dc8a304f763565f4ec0
happyjy <okwoyjy@gmail.com>
2023-05-05 23:48:49 +0900

    feat: api middlewares, response

--- nextjs-blog/src/constants/userDetail.js
+++ nextjs-blog/src/constants/userDetail.js
@@ -1,5 +1,5 @@
 export const userDetail = {
-  name: 'jyoon',
+  name: 'jyoon from userDetail.js',
   age: 34,
   height: 174,
   weight: 74,
--- nextjs-blog/src/pages/api/user-info/[uid].js
+++ nextjs-blog/src/pages/api/user-info/[uid].js
@@ -2,7 +2,18 @@ import { cookies } from 'next/dist/client/components/headers';
 
 export default function handler(req, res) {
   const { uid } = req.query;
-  // console.log('uid: ', uid)
-  // const cookies = req.cookies;
-  res.status(200).json({ name: `jyoon ${uid}` });
+  console.log('uid: ', uid);
+  const cookies = req.cookies;
+  console.log('cookies: ', cookies);
+  res
+    // 1. status: 200
+    // .status(200)
+    // .json({ name: `jyoon ${uid}`, cookies: JSON.stringify(cookies) });
+
+    // 2. status: 404
+    // .status(404)
+    // .send({ erro: 'error' })
+
+    // 3. status: 307
+    .redirect(307, '/api/user');
 }
